888f4f2dec642cc22060070130d46d08617aba0b
Dinkar <dongredinkar34@gmail.com>
2026-04-28 18:07:27 +0530

    Added HomePage code

--- src/main/java/org/zigwheels/pages/HomePage.java
+++ src/main/java/org/zigwheels/pages/HomePage.java
@@ -1,4 +1,25 @@
 package org.zigwheels.pages;
 
-public class HomePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends BasePage {
+
+    @FindBy(xpath = "//span[text()='NEW CARS']")
+    private WebElement newCarsMenu;
+
+    @FindBy(xpath = "//a[text()='Electric Cars']")
+    private WebElement electricCarsOption;
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void openElectricCars() {
+        newCarsMenu.click();
+        electricCarsOption.click();
+    }
 }
