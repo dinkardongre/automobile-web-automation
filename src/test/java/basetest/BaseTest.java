47705033b6a6155f96f543b9193215b74b396564
Dinkar <dongredinkar34@gmail.com>
2026-04-28 14:20:27 +0530

    Added base test setup and common framework utilities

--- src/test/java/basetest/BaseTest.java
+++ src/test/java/basetest/BaseTest.java
@@ -1,4 +1,43 @@
 package basetest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utilities.ConfigReader;
+import utilities.LogUtil;
+
 public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+
+        LogUtil.info("Starting browser setup");
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        // Required Chrome options
+        chromeOptions.addArguments("--disable-notifications");
+        chromeOptions.addArguments("--disable-popup-blocking");
+        chromeOptions.addArguments("--start-maximized");
+
+        // chromeOptions.addArguments("--headless=new");
+
+        driver = new ChromeDriver(chromeOptions);
+
+        LogUtil.info("Launching application URL");
+        driver.get(ConfigReader.getProperty("url"));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+        if (driver != null) {
+            LogUtil.info("Closing browser");
+            driver.quit();
+        }
+    }
 }
