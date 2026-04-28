aff5fef81a4e38e36091067b78db2a5f42cbfbac
Dinkar <dongredinkar34@gmail.com>
2026-04-28 22:49:46 +0530

    Added WaitUtils and log4j2.xml

--- src/main/java/utilities/WaitUtils.java
+++ src/main/java/utilities/WaitUtils.java
@@ -1,6 +1,28 @@
 package utilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import java.util.List;
 
 public class WaitUtils {
 
+    private WebDriverWait wait;
 
+    public WaitUtils(WebDriver driver) {
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+    }
+
+    public WebElement waitForVisibility(WebElement element) {
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public List<WebElement> waitForAllVisible(List<WebElement> elements) {
+        return wait.until(ExpectedConditions.visibilityOfAllElements(elements));
+    }
+
+    public WebElement waitForClickable(WebElement element) {
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
 }
