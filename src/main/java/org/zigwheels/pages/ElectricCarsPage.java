c6d89d9d0b26251038aca8e00c4ec1ab5f55fc44
Dinkar <dongredinkar34@gmail.com>
2026-04-29 09:23:48 +0530

    Some modification on electicCarsPage

--- src/main/java/org/zigwheels/pages/ElectricCarsPage.java
+++ src/main/java/org/zigwheels/pages/ElectricCarsPage.java
@@ -1,63 +1,59 @@
 package org.zigwheels.pages;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+import utilities.WaitUtils;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class ElectricCarsPage extends BasePage {
 
-    // Page header
-    @FindBy(xpath = "//h1[text()= 'Electric Cars']")
+    private final WaitUtils waitUtils;
+
+    @FindBy(xpath = "//h1[text()='Electric Cars']")
     private WebElement electricCarsHeader;
 
-    // Electric car cards
     @FindBy(xpath = "//div[contains(@class,'card')]")
     private List<WebElement> electricCarCards;
 
-    // Electric car names
     @FindBy(xpath = "//a[@data-track-label='-model-name']")
     private List<WebElement> carNames;
 
-    // Electric car prices
     @FindBy(xpath = "//span[@title=' Ex-Showroom Price']")
     private List<WebElement> carPrices;
 
-    // EV tag/icon
     @FindBy(css = ".rw-l-Ev")
     private List<WebElement> evTags;
 
     public ElectricCarsPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
+        this.waitUtils = new WaitUtils(driver);
     }
 
-    public boolean isElectricCarsPageDisplayed() {
-        return electricCarsHeader.isDisplayed();
+    public boolean isPageLoaded() {
+        return waitUtils.waitForVisibility(electricCarsHeader).isDisplayed();
     }
 
-    public int getElectricCarsCount() {
+    public int getListedElectricCarsCount() {
+        waitUtils.waitForAllVisible(electricCarCards);
         return electricCarCards.size();
     }
 
-    public boolean areEvTagsDisplayed() {
+    public boolean isEvTagPresent() {
         return !evTags.isEmpty();
     }
 
-    public boolean areCarNamesAndPricesDisplayed() {
+    public boolean isNameAndPriceAvailableForCars() {
         return !carNames.isEmpty() && !carPrices.isEmpty();
     }
 
-    public boolean hasDuplicateElectricCars() {
+    public boolean hasDuplicateCarNames() {
         Set<String> uniqueNames = new HashSet<>();
-
-        for (WebElement e : carNames) {
-            String name = e.getText().trim();
-            if (!uniqueNames.add(name)) {
+        for (WebElement car : carNames) {
+            if (!uniqueNames.add(car.getText().trim())) {
                 return true;
             }
         }
