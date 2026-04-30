37cb115990eef221ba2f9bb9792a08f3205ced58
Prateek <prateekmahajan03@gmail.com>
2026-04-30 12:07:49 +0530

    Created the Actions method in CommonCode

--- src/main/java/utilities/CommonCode.java
+++ src/main/java/utilities/CommonCode.java
@@ -2,9 +2,10 @@ package utilities;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
-
+import org.openqa.selenium.interactions.Actions;
 public class CommonCode {
 
     protected WebDriver driver;
@@ -13,6 +14,11 @@ public class CommonCode {
     public CommonCode(WebDriver driver)  {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+    }
+
+    protected void actionMethod(WebElement element){
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).perform();
     }
     protected void scrollIntoView(WebElement element) {
         ((JavascriptExecutor) driver)
