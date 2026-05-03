4f6c4cb31f160d76750a5f7cc812f17647921195
Dinkar <dongredinkar34@gmail.com>
2026-05-03 18:06:37 +0530

    Added ScreenshotUtil methods in TC_6 and TC_10

--- src/main/java/utilities/ScreenshotUtil.java
+++ src/main/java/utilities/ScreenshotUtil.java
@@ -9,28 +9,16 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class ScreenshotUtil {
-
     private static final String SCREENSHOT_DIR =
             System.getProperty("user.dir") + "/screenshots";
-
-    public static void captureScreenshot(WebDriver driver, String testName)
-            throws IOException {
-
+    public static void captureScreenshot(WebDriver driver, String testName) throws IOException {
         File dir = new File(SCREENSHOT_DIR);
         if (!dir.exists()) {
             dir.mkdirs();
         }
-
-        String timestamp = LocalDateTime.now()
-                .format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
-
-        File src = ((TakesScreenshot) driver)
-                .getScreenshotAs(OutputType.FILE);
-
-        File dest = new File(
-                SCREENSHOT_DIR + "/" + testName + "_" + timestamp + ".png"
-        );
-
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
+        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        File dest = new File(SCREENSHOT_DIR + "/" + testName + "_" + timestamp + ".png");
         FileHandler.copy(src, dest);
     }
 }
--- src/test/java/org/zigwheels/tests/electriccars/TC_6ElectricCars_CategoryFlow.java
+++ src/test/java/org/zigwheels/tests/electriccars/TC_6ElectricCars_CategoryFlow.java
@@ -5,17 +5,21 @@ import org.testng.annotations.Test;
 import org.zigwheels.pages.ElectricCarsPage;
 import org.zigwheels.pages.HomePage;
 import utilities.LogUtil;
+import utilities.ScreenshotUtil;
+
+import java.io.IOException;
 
 public class TC_6ElectricCars_CategoryFlow extends BaseTest {
 
     @Test
-    public void verifyElectricCarsCategoryFlow() {
+    public void verifyElectricCarsCategoryFlow() throws IOException {
         LogUtil.info("Starting TC_1ElecCarsCategoryFlow");
 
         HomePage homePage = new HomePage(driver);
         LogUtil.info("Navigating to Electric Cars from Home page");
 
         homePage.openElectricCars();
+        ScreenshotUtil.captureScreenshot(driver, "ElectricCarsPage_Loaded");
         ElectricCarsPage electricCarsPage = new ElectricCarsPage(driver);
         LogUtil.info("Validating Electric Cars page load");
 
