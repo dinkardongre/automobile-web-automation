be6d8d52cb15bcab6651f60abdc6f1dfc85713c0
Priyadarshini Panda <priyadarshinipanda700@gmail.com>
2026-05-04 09:55:01 +0530

    Added TC_22

---
+++ src/test/java/org/zigwheels/tests/usedcars/TC_22UsedCars_ValidateScrollAndSearch.java
@@ -0,0 +1,37 @@
+package org.zigwheels.tests.usedcars;
+
+import basetest.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.zigwheels.pages.HomePage;
+import org.zigwheels.pages.UsedCarsPage;
+import utilities.LogUtil;
+
+public class TC_22UsedCars_ValidateScrollAndSearch extends BaseTest {
+    @Test
+    public void validateSearchButtonAfterScroll() {
+
+        HomePage homePage = new HomePage(driver);
+        LogUtil.info("Opening Used Cars page");
+
+        homePage.openUsedCarsPage();
+        LogUtil.info("Selecting Chennai city");
+
+        UsedCarsPage usedCarsPage = new UsedCarsPage(driver);
+        usedCarsPage.selectChennaiCity();
+        LogUtil.info("Scrolling to search section");
+
+        usedCarsPage.scrollToSearchButton();
+        LogUtil.info("Validating autocomplete suggestions are displayed");
+
+        Assert.assertTrue(
+                usedCarsPage.areAutoCompleteSuggestionsDisplayed("Hyundai"),
+                    "Autocomplete suggestions are NOT displayed"
+            );
+            LogUtil.info("Autocomplete suggestions displayed successfully");
+
+        }
+    }
+
+
+
