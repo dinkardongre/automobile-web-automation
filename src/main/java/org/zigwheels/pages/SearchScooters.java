090c435842a08acf1207f3bcc4fc24a1cade5a9f
Prateek <prateekmahajan03@gmail.com>
2026-05-04 09:18:51 +0530

    Created new test case

--- src/main/java/org/zigwheels/pages/SearchScooters.java
+++ src/main/java/org/zigwheels/pages/SearchScooters.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 import utilities.CommonCode;
 import utilities.WaitUtils;
 
@@ -20,6 +21,36 @@ public class SearchScooters extends CommonCode {
     @FindBy(xpath = "//span[@title=' Ex-Showroom Price']")
     private List<WebElement> scooterPrices;
 
+
+    @FindBy(xpath = "//*[@id='manufacturers']/div/div/div/div/div[1]/ul/li[1]/div/a/div")
+    private WebElement tvsBrand;
+
+    //Scooter names after brand filter
+    @FindBy(xpath = "//a[@data-track-label='launched-model-name']")
+    private List<WebElement> scooterNames;
+
+
+    // Search Scooters section
+    @FindBy(xpath = "//*[@id='zwn-search']/div[1]/h2")
+    private WebElement searchScootersSection;
+
+
+    // Make dropdown (HTML <select>)
+    @FindBy(id = "byBrandMake")
+    private WebElement makeDropdown;
+
+    // Model dropdown (HTML <select>)
+    @FindBy(id = "byBrandModel")
+    private WebElement modelDropdown;
+
+    // Search button
+    @FindBy(xpath = "//button[text()='Search']")
+    private WebElement searchButton;
+
+    // Result page title (TVS Jupiter)
+    @FindBy(xpath = "//h1")
+    private WebElement scooterTitle;
+
     public SearchScooters(WebDriver driver) {
         super(driver);
         this.waitUtils = new WaitUtils(driver);
@@ -27,7 +58,7 @@ public class SearchScooters extends CommonCode {
     }
 
     public void clickScootersUnder40000Filter() {
-        scrollIntoView(scootersUnder40000Filter);   //USING EXISTING METHOD
+        scrollIntoView(scootersUnder40000Filter);
         waitUtils.waitForClickable(scootersUnder40000Filter).click();
     }
 
@@ -35,6 +66,44 @@ public class SearchScooters extends CommonCode {
         waitUtils.waitForCondition(driver -> scooterPrices.size() > 0);
         return scooterPrices;
     }
+
+    public void clickTVSBrand() {
+        scrollIntoView(tvsBrand);
+        waitUtils.waitForClickable(tvsBrand);
+        jsClick(tvsBrand);
+    }
+
+    public List<WebElement> getScooterNames() {
+        waitUtils.waitForCondition(driver -> scooterNames.size() > 0);
+        return scooterNames;
+    }
+
+    public void searchTVSJupiter() {
+        //Scroll first
+        scrollIntoView(searchScootersSection);
+
+        //Select Make
+        Select make = new Select(makeDropdown);
+        make.selectByVisibleText("TVS");
+
+        waitUtils.waitForCondition(driver ->
+                new Select(modelDropdown).getOptions().size() > 1
+        );
+
+        //Select Model
+        Select model = new Select(modelDropdown);
+        model.selectByVisibleText("Jupiter");
+
+        //Click Search
+        waitUtils.waitForClickable(searchButton).click();
+    }
+
+    public String getScooterTitle() {
+        return waitUtils.waitForVisibility(scooterTitle).getText();
+    }
+
+
+
 
 
 }
