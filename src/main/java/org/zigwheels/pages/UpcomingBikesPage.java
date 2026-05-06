f4aa5ebaca36f1dc1894054e11fd07e49ea797ae
Suhail Syed <syedsuhailsmoh@gamil.com>
2026-05-06 16:57:28 +0530

    Added new xpath in UpcomingBikesPage

--- src/main/java/org/zigwheels/pages/UpcomingBikesPage.java
+++ src/main/java/org/zigwheels/pages/UpcomingBikesPage.java
@@ -1,5 +1,4 @@
 package org.zigwheels.pages;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,7 +6,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.CommonCode;
 import utilities.WaitUtils;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UpcomingBikesPage extends CommonCode {
